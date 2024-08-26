5478d3ffb6577faccca6225aea57c9fe3c4d3025
Sergey Shemet <shemets@gmai.com>
2024-08-26 21:12:22 +0300

    Home work: week_1

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,71 @@
 package main
 
 import (
+	"context"
 	"fmt"
-
-	"github.com/fatih/color"
+	"github.com/brianvoe/gofakeit"
+	desc "github.com/isergos/auth/pkg/auth_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedAuthV1Server
+}
+
+func (s *server) Create(ctc context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("user info: %v", req.GetUserInfo())
+
+	return &desc.CreateResponse{Id: 3}, nil
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("user id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		User: &desc.User{
+			Id:        0,
+			Name:      gofakeit.BeerName(),
+			Email:     gofakeit.Email(),
+			Role:      0,
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("user info: %v", req.GetInfo())
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("User id: %d", req.GetId())
+
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterAuthV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: ", err)
+	}
 }
